7bb1bd44c3b2a048a808ef1ae5020b740f418fb5
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-04-17 17:55:24 -0500

    feat: :sparkles: Update MainMain component with new icons and enhance styling; add FooterMain component

---
+++ src/app/components/footermain.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export function FooterMain (){
+    return(
+        <div>
+            hola a todos
+        </div>
+    )
+}
--- src/app/components/mainmain.tsx
+++ src/app/components/mainmain.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Swords } from 'lucide-react';
+import { Swords,ShieldPlus ,Castle  } from 'lucide-react';
 
 // import Image from "next/image"
 
@@ -37,13 +37,13 @@ export function MainMain (){
                         </div>
                         
                         <div>
-                            <Swords style={ {width: "5vw",height:"5vw",background:"#252525",padding:"1vw",borderRadius:"1vw" }} />
+                            <ShieldPlus  style={ {width: "5vw",height:"5vw",background:"#252525",padding:"1vw",borderRadius:"1vw" }} />
                             <h1>Define Your Class</h1>
                             <p>Become a mighty Warrior, a cunning Rogue, a wise Wizard, or any other class that fits your playstyle.</p>
                         </div>
                         
                         <div>
-                        <Swords style={ {width: "5vw",height:"5vw",background:"#252525",padding:"1vw",borderRadius:"1vw" }} />
+                            <Castle  style={ {width: "5vw",height:"5vw",background:"#252525",padding:"1vw",borderRadius:"1vw" }} />
                             <h1>Customize Abilities</h1>
                             <p>Tailor your character s strengths and weaknesses with customizable stats and special abilities.</p>    
                         </div>
