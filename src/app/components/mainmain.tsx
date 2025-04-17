b3cddf76bf87c37d91f8366e65d237eb8359420f
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-04-17 17:23:27 -0500

    feat: :sparkles: Add lucide-react dependency and enhance MainMain component with new sections and styles

--- src/app/components/mainmain.tsx
+++ src/app/components/mainmain.tsx
@@ -1,6 +1,7 @@
 "use client"
+import { Swords } from 'lucide-react';
 
-
+// import Image from "next/image"
 
 export function MainMain (){
     return(
@@ -18,6 +19,43 @@ export function MainMain (){
                     </div>
                 </article>
             </section>
+            <section>
+                <article>
+                    <div>
+                        <h1>Endless Possibilities</h1>
+                        
+                        <p>Customize every aspect of your character from race and class to equipment and special abilities.</p>
+                    </div>
+
+                    <div>
+                        
+                        <div>
+                            <Swords style={ {width: "5vw",height:"5vw",background:"#252525",padding:"1vw",borderRadius:"1vw" }} />
+                            <h1>Choose Your Race</h1>
+                            <p>Select from a variety of fantasy races including Humans, Elves, Dwarves, and more.</p>
+                       
+                        </div>
+                        
+                        <div>
+                            <Swords style={ {width: "5vw",height:"5vw",background:"#252525",padding:"1vw",borderRadius:"1vw" }} />
+                            <h1>Define Your Class</h1>
+                            <p>Become a mighty Warrior, a cunning Rogue, a wise Wizard, or any other class that fits your playstyle.</p>
+                        </div>
+                        
+                        <div>
+                        <Swords style={ {width: "5vw",height:"5vw",background:"#252525",padding:"1vw",borderRadius:"1vw" }} />
+                            <h1>Customize Abilities</h1>
+                            <p>Tailor your character s strengths and weaknesses with customizable stats and special abilities.</p>    
+                        </div>
+
+
+                    </div>
+
+                </article>
+            </section>
+
+
+
         </main>
     )
     
